795a9e989c5849ed8ff5cd5309f1957c7675ee9f
Kanishka0306 <kanishka031629@gmail.com>
2026-07-22 05:10:30 +0530

    Time: 0 ms (100%), Space: 12.7 MB (5.99%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -9,7 +9,7 @@ public:
         int low = 1;
         int high = n-2;
         while(low<= high){
-            int mid = (low +high)/2;
+            int mid = low + (high-low)/2;
             if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
                 return mid;
             }
