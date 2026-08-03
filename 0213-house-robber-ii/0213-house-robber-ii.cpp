a5f27974c68ca9d05a276e7cc8213c7fa3766d59
Kanishka0306 <kanishka031629@gmail.com>
2026-08-03 23:35:57 +0530

    Time: 0 ms (100%), Space: 11 MB (7.63%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,44 +1,35 @@
 class Solution {
 public:
-
-    int solve(int i, vector<int>& nums, vector<int>& dp)
-    {
-        if(i == 0)
-            return nums[0];
-        if(i < 0)
-            return 0;
-        if(dp[i] != -1)
-            return dp[i];
-
-        int pick = nums[i] + solve(i-2, nums, dp);
-        int notPick = solve(i-1, nums, dp);
-
-        return dp[i] = max(pick, notPick);
-    }
-
-    int rob(vector<int>& nums)
-    {
+    int solve(vector<int>& nums){
         int n = nums.size();
-        if(n == 1)
-            return nums[0];
+        vector<int> dp(n);
 
-        vector<int> arr1, arr2;
+        dp[0] = nums[0];
+        for(int i=1; i<n;i++){
+            int pick = nums[i];
 
-        for(int i = 0; i < n; i++)
-        {
-            if(i != 0)
-                arr1.push_back(nums[i]);
-
-            if(i != n-1)
-                arr2.push_back(nums[i]);
+            if(i>1) pick += dp[i-2];
+            int notpick = dp[i-1];
+            dp[i] = max(pick, notpick);
         }
+        return dp[n-1];
+    }
+    
 
-        vector<int> dp1(arr1.size(), -1);
-        vector<int> dp2(arr2.size(), -1);
+    int rob(vector<int>& nums) {
+        int n = nums.size();
 
-        int ans1 = solve(arr1.size()-1, arr1, dp1);
-        int ans2 = solve(arr2.size()-1, arr2, dp2);
+        if(n==1) return nums[0];
+        vector<int> arr1, arr2;
+        for(int i=0;  i<n; i++){
+            if (i!= 0 ) arr1.push_back(nums[i]);
 
-        return max(ans1, ans2);
+            if (i!= n-1 ) arr2.push_back(nums[i]);
+
+        }
+        int ans1 = solve(arr1);
+        int ans2 = solve(arr2);
+        return max(ans1 , ans2);
+
     }
 };
